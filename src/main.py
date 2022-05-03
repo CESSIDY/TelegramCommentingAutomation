7ec5d4f7c9ca87f74256e4925f45d124d1fca4b2
rupa_vi <rupa_vi@groupbwt.com>
2022-05-03 15:25:54 +0200

    - feat(utils): added additional configuration files and method; - feat(main): all low-abstraction logic moved to utils;

--- src/main.py
+++ src/main.py
@@ -5,36 +5,30 @@ from loaders.accounts import JsonAccountsLoader
 from loaders.channels import JsonChannelsLoader
 from loaders.comments import JsonCommentsLoader
 from models import AccountsLoaderModel
-from utils import get_configurations
+from utils import session_files_path, get_proxy_configurations, configure_logging
 from workers.commentator import Commentator
 
-logging.basicConfig(filename="telegram_commentator.log",
-                        filemode='a',
-                        format='%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
-                        datefmt='%Y-%m-%d:%H:%M:%S',
-                        level=logging.INFO)
+configure_logging()
 logger = logging.getLogger(__name__)
 
 
 def main():
-    config = get_configurations()
-
     comments_loader = JsonCommentsLoader()
     channels_loader = JsonChannelsLoader()
     accounts_loader = JsonAccountsLoader()
     account_models = accounts_loader.get_all()
 
-    account_models = check_and_init_sessions(account_models, config)
-    run_commentator_for_all_accounts(account_models, config, comments_loader, channels_loader)
+    account_models = check_and_init_sessions(account_models)
+    run_commentator_for_all_accounts(account_models, comments_loader, channels_loader)
 
 
-def check_and_init_sessions(account_models, config) -> [AccountsLoaderModel]:
+def check_and_init_sessions(account_models) -> [AccountsLoaderModel]:
     temp_account_models = list()
 
     for account in account_models:
         print(f"Login for: {account.username}")
         try:
-            client = run_and_return_client(config, account)
+            client = run_and_return_client(account)
             client.disconnect()
             temp_account_models.append(account)
         except Exception as e:
@@ -44,39 +38,25 @@ def check_and_init_sessions(account_models, config) -> [AccountsLoaderModel]:
     return temp_account_models
 
 
-def run_commentator_for_all_accounts(account_models, config, comments_loader, channels_loader):
+def run_commentator_for_all_accounts(account_models, comments_loader, channels_loader):
     for account in account_models:
         print(f"Start processing for {account.username}")
-        client = run_and_return_client(config, account)
+        client = run_and_return_client(account)
         commentator = Commentator(client, comments_loader, channels_loader)
         commentator.run_until_complete()
         client.disconnect()
         print(f"Finish processing for {account.username}")
 
 
-def run_and_return_client(config, account_model: AccountsLoaderModel):
-    config_file_path = os.path.abspath(os.path.join(os.path.realpath(__file__), '..', "sessions"))
+def run_and_return_client(account_model: AccountsLoaderModel):
+    session_path = os.path.join(session_files_path, account_model.username)
 
-    api_id = account_model.api_id
-    api_hash = account_model.api_hash
-    username = account_model.username
-
-    session_path = os.path.join(config_file_path, username)
-
-    is_proxy_enabled = int(config['Proxy']['proxy_enabled'])
-
-    if is_proxy_enabled:
-        proxy = {
-            'proxy_type': config['Proxy']['proxy_type'],  # (mandatory) protocol to use
-            'addr': config['Proxy']['addr'],  # (mandatory) proxy IP address
-            'port': config['Proxy']['port'],  # (mandatory) proxy port number
-            'username': config['Proxy']['username'],  # (optional) username if the proxy requires auth
-            'password': config['Proxy']['password'],  # (optional) password if the proxy requires auth
-            'rdns': True  # (optional) whether to use remote or local resolve, default remote
-        }
-        client = TelegramClient(session_path, api_id, api_hash, proxy=proxy)
+    proxy = get_proxy_configurations()
+    if proxy:
+        client = TelegramClient(session_path, account_model.api_id, account_model.api_hash, proxy=proxy)
     else:
-        client = TelegramClient(session_path, api_id, api_hash)
+        client = TelegramClient(session_path, account_model.api_id, account_model.api_hash)
+
     client.start()
     return client
 
--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,2 +1,3 @@
-from .config import get_configurations
+from .config import get_configurations, get_proxy_configurations, configure_logging
 from .media_path import images_dir, documents_dir, video_dir
+from .session_path import session_files_path
